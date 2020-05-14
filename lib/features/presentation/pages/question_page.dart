41779f6231391d4c748ed13f911f90c6b2ab596d
Florian G <florian.grundmann@freenet.de>
2020-05-14 16:08:41 +0700

    Added QuestionPage skeleton.

---
+++ lib/features/presentation/pages/question_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuestionPage extends StatefulWidget {
+  QuestionPage({Key key}) : super(key: key);
+
+  @override
+  _QuestionPageState createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Question Page'),
+    );
+  }
+}
