effe88f9a3f8d71f566411a716d9445044276236
Florian G <florian.grundmann@freenet.de>
2020-05-15 13:25:34 +0700

    Removed Android status bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'features/questions/presentation/pages/question_page.dart';
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       home: Scaffold(
         body: QuestionPage(),
