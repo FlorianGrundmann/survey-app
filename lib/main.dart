80d3628a9bac1586fcd081f54e0a68bf7531d046
Florian G <florian.grundmann@freenet.de>
2020-05-26 11:59:55 +0700

    Locked screen orientation to portrait.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'injection_container.dart' as di;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
