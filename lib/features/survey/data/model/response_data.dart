19c59be6a8872f55418344c50984416078047f57
Florian G <florian.grundmann@freenet.de>
2020-05-28 18:02:48 +0700

    Created ResponseInteger.

---
+++ lib/features/survey/data/model/response_data.dart
@@ -0,0 +1,11 @@
+class ResponseInteger {
+  final String questionId;
+  final String responseId;
+  final int response;
+
+  ResponseInteger({
+    this.questionId,
+    this.responseId,
+    this.response,
+  });
+}
