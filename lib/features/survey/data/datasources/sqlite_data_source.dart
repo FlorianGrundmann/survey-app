4010f0b19dbee6d7ca6a4280e0d2120912d7a4a5
Florian G <florian.grundmann@freenet.de>
2020-05-30 10:57:18 +0700

    Added the hardcoded questions into the database.

--- lib/features/survey/data/datasources/sqlite_data_source.dart
+++ lib/features/survey/data/datasources/sqlite_data_source.dart
@@ -6,8 +6,8 @@ import 'package:survey_app/features/survey/data/model/response_data.dart';
 class SqliteDataSource implements LocalSurveyDataSource {
   Future<Database> _database;
 
-  SqliteDataSource() {
-    _database = _openDatabase();
+  SqliteDataSource([Future<Database> database]) {
+    _database = database ?? _openDatabase();
   }
 
   @override
@@ -16,7 +16,7 @@ class SqliteDataSource implements LocalSurveyDataSource {
     for (ResponseData response in surveyData) {
       await db.insert(
         'responses',
-        toMap(response),
+        _toMap(response),
         conflictAlgorithm: ConflictAlgorithm.replace,
       );
     }
@@ -33,14 +33,31 @@ class SqliteDataSource implements LocalSurveyDataSource {
       join(await getDatabasesPath(), 'survey_app.db'),
       onCreate: (db, version) {
         return db.execute(
-          "CREATE TABLE responses(response_id TEXT PRIMARY KEY, response TEXT, responder_id TEXT, question_id TEXT);",
+          'CREATE TABLE questions(question_id TEXT PRIMARY KEY, question_text TEXT, type TEXT);'
+          'CREATE TABLE responses(response_id TEXT PRIMARY KEY, response TEXT, responder_id TEXT, question_id TEXT, FOREIGN KEY(question_id) REFERENCES questions(question_id));'
+          'INSERT INTO questions(question_id, question_text, type) '
+          'VALUES '
+          '("a726506a-a0cc-11ea-bb37-0242ac130002", "Der Ablauf der Anästhesie sollte besser erläutert werden.", "four_point_rating")'
+          '("a72653ee-a0cc-11ea-bb37-0242ac130002", "Es wurde zu wenig über die Risiken der bevorstehenden Anästhesie informiert.", "four_point_rating")'
+          '("a72654e8-a0cc-11ea-bb37-0242ac130002", "Ich empfand das Video insgesamt als verständlich.", "four_point_rating")'
+          '("a72655b0-a0cc-11ea-bb37-0242ac130002", "Das Video war insgesamt von guter Qualität.", "four_point_rating")'
+          '("a726566e-a0cc-11ea-bb37-0242ac130002", "Das Video war zu lang.", "four_point_rating")'
+          '("a7265722-a0cc-11ea-bb37-0242ac130002", "Die Bilder und Texte halfen mir den Information besser zu folgen.", "four_point_rating")'
+          '("a7265bc8-a0cc-11ea-bb37-0242ac130002", "Es war zu viel Text auf den Folien.", "four_point_rating")'
+          '("a7265cfe-a0cc-11ea-bb37-0242ac130002", "Die Stimme des Sprechers war angenehm.", "four_point_rating")'
+          '("a7265dbc-a0cc-11ea-bb37-0242ac130002", "Der Sprecher sprach zu schnell oder undeutlich.", "four_point_rating")'
+          '("a7265e7a-a0cc-11ea-bb37-0242ac130002", "Ich habe das Gefühl ernst genommen zu werden.", "four_point_rating")'
+          '("a7265f2e-a0cc-11ea-bb37-0242ac130002", "Ich wünsche mir mehr videounterstützte Aufklärung.", "four_point_rating")'
+          '("a7265fe2-a0cc-11ea-bb37-0242ac130002", "Wartezeiten konnten durch die videounterstützte Aufklärung sinnvoll genutzt werden.", "four_point_rating")'
+          '("a72662f8-a0cc-11ea-bb37-0242ac130002", "Ich bin mit der videounterstützten Aufklärung zufrieden.", "four_point_rating")'
+          ';',
         );
       },
-      version: 1,
+      version: 2,
     );
   }
 
-  Map<String, String> toMap(ResponseData response) {
+  Map<String, String> _toMap(ResponseData response) {
     return {
       'response_id': response.responseId,
       'response': response.response,
