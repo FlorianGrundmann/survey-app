8e38798344f73b3dc55c2273e4f5378f07a02cd6
Florian G <florian.grundmann@freenet.de>
2020-06-09 10:43:50 +0700

    Added navigation drawer.

---
+++ lib/features/survey/presentation/widgets/nav_drawer.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../fixed_values/survey_sizes.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Text _text(String text) {
+      return Text(
+        text,
+        style: TextStyle(
+          fontSize:
+              SurveySizes.scaledHeight(SurveySizes.regularFontSize, context),
+        ),
+      );
+    }
+
+    return Drawer(
+      child: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: Image(
+                fit: BoxFit.fitWidth,
+                image: AssetImage('assets/images/phone-doctor.jpg'),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.input),
+              title: _text('Neue Umfrage'),
+              onTap: () => {Navigator.of(context).pop()},
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: _text('Exportiere Antworten'),
+              onTap: () => {Navigator.of(context).pop()},
+            ),
+            ListTile(
+              leading: Icon(Icons.lock),
+              title: _text('Exportiere Fragen'),
+              onTap: () => {Navigator.of(context).pop()},
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: _text('Über App'),
+              onTap: () => {Navigator.of(context).pop()},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
